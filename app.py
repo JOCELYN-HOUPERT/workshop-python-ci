0a74355c9dfd559b849bd56aff1b1667c76b38ea
JOCELYN-HOUPERT <jocelyn.houpert@viacesi.fr>
2026-04-29 14:01:57 +0200

    Termine la configuration de la calculatrice et de la CI

---
+++ app.py
@@ -0,0 +1,8 @@
+"""Module de calculatrice simple."""
+
+def add(first, second):
+    """Effectue l'addition de deux nombres."""
+    return first + second
+
+if __name__ == "__main__":
+    print(add(2, 5))
